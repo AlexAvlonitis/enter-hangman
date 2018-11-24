77eece8c3ff605c469803aee869f8daab40f4b80
Alex <avlonitis@msn.com>
2018-11-24 16:12:41 +0000

    extract random word

--- src/components/word/WordInput.js
+++ src/components/word/WordInput.js
@@ -7,18 +7,40 @@ class WordInput extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { wordsArray: null }
+    this.state = { wordsArray: null, pickedWord: null }
   }
 
   componentDidMount = () => {
     const wordsArray = readFile(wordsText);
-    this.setState({ wordsArray });
+    this.setState({
+      wordsArray
+    }, () => {
+      this.randomWordPicker();
+    });
+  }
+
+  randomWordPicker = () => {
+    const {wordsArray} = this.state;
+    const pickedWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    this.setState({pickedWord});
+  }
+
+  renderWord = () => {
+    return (
+      <div>
+        <p> Welcome </p>
+        <p> This is the picked word: {this.state.pickedWord}</p>
+      </div>
+    )
   }
 
   render() {
     return (
       <div className="WordInput">
-        Welcome to hangman
+        { this.state.wordsArray ?
+          this.renderWord() :
+          <p>Loading...</p>
+        }
       </div>
     );
   }
