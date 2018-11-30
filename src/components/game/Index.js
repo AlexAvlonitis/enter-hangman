c57ff6fcda11c71dab51dbcd281be00105b51409
Spiros Avlonitis <spirosa84@hotmail.com>
2018-11-30 05:34:35 +0200

    state lifting

--- src/components/game/Index.js
+++ src/components/game/Index.js
@@ -11,48 +11,117 @@ class Index extends Component {
     super(props);
 
     this.state = {
-      pickedWord: null,
+      failedLetters: [],
+      word: null,
       tries: 6,
       isLoading: true
     }
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    axios.get(wordsText).then( res => {
-      this.setState({
-         pickedWord: this.randomWordPicker(res.data.split("\n")),
-         isLoading: false,
-      });
-    })
+
+  isItOverYet() {
+    const {word, tries} = this.state;
+    if (tries === 0) {
+      alert(`Game Over! \nThe word was: ${word}`);
+      window.location.reload();
+    }
   }
 
-  randomWordPicker(wordsArray) {
-    return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+  addToFailedLetters(value) {
+    const {failedLetters} = this.state;
+    failedLetters.push(value);
+    this.setState({failedLetters: failedLetters})
   }
 
-  reduceTries = () => {
+  revealLetters (indexes) {
+    const {word} = this.state;
+    indexes.map( index => 
+      document.querySelector(`[data-index='${index}']`).value = word[index]
+    );
+  }
+
+  getAllIndexes(value) {
+    const {word} = this.state;
+    return (word.slice(1, word.length-1)
+      .map( (c, i) => c === value ? i+1 : null)
+      .filter( e => e )
+    ); 
+  }
+
+  checkLetter(value) {
+    const indexes = this.getAllIndexes(value)
+    if(indexes.length > 0) {
+      this.revealLetters(indexes);
+      this.checkGameStatus();
+    } else {
+      this.addToFailedLetters(value);
+      this.reduceTries();
+    }
+  }
+
+  reduceTries() {
     const {tries} = this.state;
     this.setState({ tries: tries - 1 }, () => {
       this.isItOverYet();
     });
   }
 
-  isItOverYet() {
-    if (this.state.tries === 0) {
-      alert(`Game Over! \nThe word was: ${this.state.pickedWord}`);
+  checkGameStatus() {
+    if (this.wordMatch()) {
+      alert("You won!");
       window.location.reload();
     }
   }
 
+  wordMatch() {
+    let currentWord = [];
+    const fields = document.getElementsByClassName('WordInput-bottom-border-input');
+
+    for (let i = 0; i < fields.length; i++)
+      currentWord.push(fields[i].value);
+
+    return (currentWord.join('') === this.state.word.join('')) ? true : false
+  }
+
+  handleChange(event) {
+    this.checkLetter(event.target.value);
+    document.getElementById('letter-input').value = '';
+  }
+
+  randomWordPicker(wordsArray) {
+    return wordsArray[Math.floor(Math.random() * wordsArray.length)].split('');
+  }
+  
+  componentDidMount() {
+    axios.get(wordsText).then( res => {
+      this.setState({
+         word: this.randomWordPicker(res.data.split("\n")),
+         isLoading: false,
+      });
+    })
+  }
+
   render() {
-    const {tries, pickedWord, isLoading} = this.state;
+    const {tries, word, isLoading, failedLetters} = this.state;
     
-    return (
+    return ( word &&
       <div className="Game-Index">
         <div className="Game-Inputs">
+          <p>
+            <button onClick={() => alert(word.join(''))}>
+                Cheat, show word
+            </button>
+          </p>
           <WordInputWithLoading {
-            ...{isLoading, pickedWord, tries, reduceTries: this.reduceTries}}
+            ...{ isLoading, word, tries, failedLetters,
+                reduceTries: this.reduceTries,
+                handleChange: this.handleChange
+              }
+            }
           />
+          <p> Letters that don't exist: </p>
+          <p> {failedLetters.join(', ')} </p>
         </div>
         <div className="Game-Canvas">
           <CanvasIndex {...{tries}} />
--- src/components/game/WordInput.js
+++ src/components/game/WordInput.js
@@ -3,93 +3,22 @@ import PropTypes from 'prop-types'
 import './WordInput.css';
 
 export default class WordInput extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      failedLetters: [],
-      word: this.props.pickedWord.split('')
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  addToFailedLetters(value) {
-    const {failedLetters} = this.state;
-    failedLetters.push(value);
-    this.setState({failedLetters: failedLetters})
-  }
-
-  getAllIndexes(value) {
-    const {word} = this.state;
-    return (word.slice(1, word.length-1)
-      .map( (c, i) => c === value ? i+1 : null)
-      .filter( e => e )
-    ); 
-  }
-
-  checkLetter(value) {
-    const indexes = this.getAllIndexes(value)
-    if(indexes.length > 0) {
-      this.revealLetters(indexes);
-      this.checkGameStatus();
-    } else {
-      this.addToFailedLetters(value);
-      this.reduceTries();
-    }
-  }
-
-  revealLetters (indexes) {
-    indexes.map( index => {
-      this.findInput(index).value = this.state.word[index];
-    });
-  }
-
-  findInput(index) {
-    return document.querySelector(`[data-index='${index}']`);
-  }
-
-
-  reduceTries() {
-    this.props.reduceTries()
-  }
-
-  checkGameStatus() {
-    if (this.wordMatch()) {
-      alert("You won!");
-      window.location.reload();
-    }
-  }
-
-  wordMatch() {
-    let currentWord = [];
-    const fields = document.getElementsByClassName('WordInput-bottom-border-input');
-
-    for (let i = 0; i < fields.length; i++)
-      currentWord.push(fields[i].value);
-
-    return (currentWord.join('') === this.state.word.join('')) ? true : false
-  }
-
-  handleChange(event) {
-    this.checkLetter(event.target.value);
-    document.getElementById('letter-input').value = '';
-  }
-
-  renderInputs = (word, index) => {
+  renderInputs = word => (char, index) => {
     if (index === 0) {
       return (
         <input
           className="WordInput-bottom-border-input"
-          value={word}
+          value={char}
           disabled
           key={index}
         />
       )
-    } else if (this.state.word.length - 1 === index) {
+    } else if (word.length - 1 === index) {
       return (
         <input
           className="WordInput-bottom-border-input"
-          value={word}
+          value={char}
           disabled
           key={index}
         />
@@ -106,42 +35,26 @@ export default class WordInput extends Component {
     }
   }
 
-  renderInput = (word) => {
-    return (
-      <div>
-        { word.map(this.renderInputs) }
+  render() {
+    const {word, failedLetters, handleChange} = this.props;
+    console.log(word)
+    return (   
+      <div className="WordInput-align-center">
+        { word.map(this.renderInputs(word)) }
         <div>
           <h3>Enter a letter below</h3>
           <input
             className="WordInput-enter-letter-input"
             id='letter-input'
             autoFocus
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
         </div>
-      </div>
-    )
-  }
-
-  render() {
-    const {word, failedLetters} = this.state;
-    const {pickedWord} = this.props;
-    
-    return (  pickedWord && 
-      <div className="WordInput-align-center">
-        <p>
-          <button onClick={() => alert(pickedWord)}>
-                Cheat, show word
-          </button>
-        </p>
-          { this.renderInput(word) }
-        <p> Letters that don't exist: </p>
-        <p> {failedLetters.join(', ')} </p>
       </div>
     );
   }
 }
 
 WordInput.propTypes = {
-  pickedWord: PropTypes.string.isRequired,
+  word: PropTypes.array.isRequired,
 }
